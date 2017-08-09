ec9bac7e5781e74b53b320fb51bfa6ee683bf09b
Phil Ratcliffe <phil@philratcliffe.co.uk>
2017-08-09 09:59:44 +0100

    Refactoring and created a simple program to add many certs to AD for testing.

--- DirectoryCertChecker/DirectoryCertChecker.cs
+++ DirectoryCertChecker/DirectoryCertChecker.cs
@@ -16,6 +16,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.DirectoryServices;
 using System.Linq;
 using System.Reflection;
@@ -29,78 +31,63 @@ namespace DirectoryCertChecker
     internal class DirectoryCertChecker
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static int _certCount;
 
         private static void Main(string[] args)
         {
-            if (args.Length > 0)
-            {
-                Log.Warn("Unexpected argument passed.");
-                Console.WriteLine("This program does not expect any arguments");
-            }
-
-            var server = Config.GetAppSetting("server");
-            var baseDn = Config.GetAppSetting("searchBaseDn");
-
-
-            Log.Info("DirectoryCertChecker has started.");
-
             try
             {
-                var cp = new CertProcessor(server, baseDn);
-                cp.ProcessCerts();
+                if (args.Length > 0)
+                {
+                    Log.Warn("Unexpected argument passed.");
+                    Console.WriteLine("This program does not expect any arguments");
+                }
+
+                Log.Info("DirectoryCertChecker has started.");
+
+                var server = Config.GetAppSetting("server");
+                var baseDn = Config.GetAppSetting("searchBaseDn");
+
+                try
+                {
+                    var directoryCertSearcher = new DirectoryCertSearcher();
+                    var searchResultProcessor = new SearchResultProcessor();
+
+                    foreach (var result in directoryCertSearcher.Search(server, baseDn))
+                        _certCount += searchResultProcessor.ProcessSearchResult(result);
+                }
+                catch (COMException ce)
+                {
+                    var msg = $"There was a problem trying to connect to your LDAP server at {server}.";
+                    Console.WriteLine($"{msg} See the DirectoryCertChecker.log file for more details.");
+                    Log.Error(msg, ce);
+                }
+                Console.WriteLine("Found " + _certCount + " certs");
             }
-            catch (COMException ce)
+            catch (ConfigurationErrorsException cee)
             {
-                var msg = $"There was a problem trying to connect to your LDAP server at {server}.";
-                Console.WriteLine($"{msg} See the DirectoryCertChecker.log file for more details.");
-                Log.Error(msg, ce);
+                Console.WriteLine($"Error reading the config file - {cee.Message}");
+                Log.Error("Error reading the config file.", cee);
             }
             catch (Exception ex)
             {
-                Log.Info("There was an error. Check the DirectoryCertChecker.log file for more details.");
+                Console.WriteLine("There was an error. Check the DirectoryCertChecker.log file for more details.");
                 Log.Error("Top level exception caught. ", ex);
             }
         }
     }
 
-    internal class CertProcessor
+    internal class DirectoryCertSearcher
     {
-        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        private readonly string _searchBaseDn;
-        private readonly string _server;
-
-        private int _certCount;
-
-        /// <summary>
-        ///     Class constructor.
-        /// </summary>
-        /// <param name="server">
-        ///     The server to search.
-        /// </param>
-        /// ///
-        /// <param name="searchBaseDn">
-        ///     The DN of the entry where you would like the search to begin. An empty string equals root.
-        /// </param>
-        internal CertProcessor(string server, string searchBaseDn)
+        public IEnumerable<SearchResult> Search(string server, string searchBase)
         {
-            _server = server;
-            _searchBaseDn = searchBaseDn;
-            _certCount = 0;
-        }
-
-        /// <summary>
-        ///     Searches Microsoft Active Directory for certificates in the userCertificate attribute and processes them.
-        /// </summary>
-        internal void ProcessCerts()
-        {
-            using (var searchBaseEntry = new DirectoryEntry("LDAP://" + _server + "/" + _searchBaseDn))
+            using (var searchBaseEntry = new DirectoryEntry("LDAP://" + server + "/" + searchBase))
             {
                 searchBaseEntry.AuthenticationType =
-                    AuthenticationTypes.None; // Use this setting for anonymous simple authentication and simple authentication
+                    AuthenticationTypes
+                        .None; // Use this setting for anonymous simple authentication and simple authentication
                 searchBaseEntry.Username = Config.GetAppSetting("ldapUsername", null);
                 searchBaseEntry.Password = Config.GetAppSetting("ldapPassword", null);
-
                 using (var findCerts = new DirectorySearcher(searchBaseEntry))
                 {
                     findCerts.SearchScope = SearchScope.Subtree;
@@ -116,34 +103,31 @@ namespace DirectoryCertChecker
 
                     using (var results = findCerts.FindAll())
                     {
-                        Log.Debug($"Directory search returned {results.Count} directory entries.");
                         foreach (SearchResult result in results)
-                            ProcessSearchResult(result);
+                            yield return result;
                     }
                 }
             }
-
-            Console.WriteLine("Found " + _certCount + " certs");
         }
+    }
 
-        /// <summary>
-        ///     Takes the search result for a specific Active Directory entry and writes out the certificate expiry information.
-        ///     If an entry has multiple certificate entries (I assume this is because old certs are sometimes
-        ///     left in the directory entry), it writes out only the most recent expiry date.
-        /// </summary>
-        /// <param name="result">
-        ///     The result param encapsulates a node in the Active Directory Domain Services hierarchy
-        ///     that is returned during a search through DirectorySearcher.
-        /// </param>
-        private void ProcessSearchResult(SearchResult result)
+    internal class SearchResultProcessor
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+
+        public int ProcessSearchResult(SearchResult result)
         {
-            var distinguisjedName = Uri.UnescapeDataString(new Uri(result.Path).Segments.Last());
-            Log.Debug($"{distinguisjedName} has: {result.Properties["UserCertificate"].Count} certs.");
-            Console.Write($"{distinguisjedName}");
+            var entryDn = Uri.UnescapeDataString(new Uri(result.Path).Segments.Last());
+            Log.Debug($"{entryDn} has: {result.Properties["UserCertificate"].Count} certs.");
+            Console.Write($"{entryDn}");
+            var certCount = 0;
 
 
             // Intialise expiry date
             var latestExpiryDate = Epoch;
+            
 
             var certificatesAsBytes = result.Properties["UserCertificate"];
             foreach (byte[] certificateBytes in certificatesAsBytes)
@@ -154,7 +138,7 @@ namespace DirectoryCertChecker
                     var difference = certificate.NotAfter - latestExpiryDate;
                     if (difference.TotalMilliseconds > 0)
                         latestExpiryDate = certificate.NotAfter;
-                    _certCount += 1;
+                    certCount += 1;
                 }
                 catch (CryptographicException ce)
                 {
@@ -170,6 +154,7 @@ namespace DirectoryCertChecker
             {
                 Console.WriteLine($" (Expires: {latestExpiryDate.ToShortDateString()})");
             }
+            return certCount;
         }
     }
 }
